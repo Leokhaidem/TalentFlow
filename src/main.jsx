9dd6c1637bc7b96d9719469860c7ce09357022ff
Leokhaidem <leokhaidemgit@gmail.com>
2025-09-20 23:12:46 +0530

    fix-deploy-v1

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import { worker } from "./lib/msw";
 import "./index.css";
 import App from "./App.jsx";
+
 async function enableMocks() {
-  if (import.meta.env.DEV) {
+  // Run in dev OR if REACT_APP_ENABLE_MSW=true
+  if (import.meta.env.DEV || import.meta.env.VITE_ENABLE_MSW === "true") {
     await worker.start({
       onUnhandledRequest: "bypass",
     });
     console.log("🔧 Mock Service Worker started");
   }
 }
+
 enableMocks().then(() => {
   createRoot(document.getElementById("root")).render(
     <StrictMode>
